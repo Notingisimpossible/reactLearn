16ca0adaa73a5f9338d8193e4f70c9024e09ecab
Notingisimpossible <2421665649@qq.com>
2020-03-12 23:21:16 +0800

    简书

---
+++ jianshu/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return(
+    <div className="dell">
+      hello
+    </div>
+  )
+}
+
+export default App
---
+++ jianshu/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {Globalstyle} from './style.js'
+
+ReactDOM.render(
+  <div>
+    <Globalstyle />
+    <App />
+  </div>, 
+  document.getElementById('root')
+)
