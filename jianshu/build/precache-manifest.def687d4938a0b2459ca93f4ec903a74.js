0164a8f972015494036304ce4e65026e88679504
Notingisimpossible <2421665649@qq.com>
2020-04-07 17:15:05 +0800

    简书

---
+++ jianshu/build/precache-manifest.def687d4938a0b2459ca93f4ec903a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5eebadacfc71e41a0fd20574d9fb7b5",
+    "url": "/reactLearn/jianshu/build/index.html"
+  },
+  {
+    "revision": "1e5262598d6258f6a49f",
+    "url": "/reactLearn/jianshu/build/static/js/2.a0ad7985.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactLearn/jianshu/build/static/js/2.a0ad7985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f9a8aea0152449a039",
+    "url": "/reactLearn/jianshu/build/static/js/3.886ce430.chunk.js"
+  },
+  {
+    "revision": "0edec1b02be7487e0229",
+    "url": "/reactLearn/jianshu/build/static/js/main.e068c697.chunk.js"
+  },
+  {
+    "revision": "0549bad2119aaf6d6c66",
+    "url": "/reactLearn/jianshu/build/static/js/runtime-main.eade1244.js"
+  }
+]);
--- jianshu/build/service-worker.js
+++ jianshu/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e017fcc91f36cf8ed2aa9af16eec09bf.js"
+  "/reactLearn/jianshu/build/precache-manifest.def687d4938a0b2459ca93f4ec903a74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactLearn/jianshu/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
