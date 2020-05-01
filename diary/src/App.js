5169ea5bc3745b5948949a8729b531f2a4599b43
notingisimpossible <2421665649@qq.com>
2020-05-01 11:37:30 +0800

    手撸日记

---
+++ diary/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from 'antd-mobile'
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">primary</Button>
+    </div>
+  );
+}
+
+export default App;
