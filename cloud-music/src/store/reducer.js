a50d7e466ef9ccbaee54d73ec323db8071cbd734
notingisimpossible <2421665649@qq.com>
2020-08-17 23:22:06 +0800

    状态管理

--- cloud-music/src/store/reducer.js
+++ cloud-music/src/store/reducer.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux-immutable'
+import { reducer as recommendReducer } from '../application/Recommend/store'
+import {reducer as singerReducer} from '../application/Singer/store'
+import { reducer as rankReducer } from '../application/Rank/store'
 
 export default combineReducers({
-  
+  recommend: recommendReducer,
+  singers: singerReducer,
+  rank: rankReducer
 })
