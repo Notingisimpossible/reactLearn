786004d17ef6e16a6af024678c64e832279b5278
notingisimpossible <2421665649@qq.com>
2020-08-17 23:21:18 +0800

    列表等组件

--- cloud-music/src/components/list/index.js
+++ cloud-music/src/components/list/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { getCount } from "../../api/utils";
-import { 
+import {
   ListWrapper,
   ListItem,
   List
 } from './style';
+import LazyLoad from 'react-lazyload'
 
-function RecommendList (props) {
+function RecommendList(props) {
   return (
     <ListWrapper>
       <h1 className="title"> 推荐歌单 </h1>
       <List>
         {
-          props.recommendList.map ((item, index) => {
+          props.recommendList.map((item, index) => {
             return (
               <ListItem key={item.id + index}>
                 <div className="img_wrapper">
                   <div className="decorate"></div>
-                    {/* 加此参数可以减小请求的图片资源大小 */}
-                    <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music"/>
+                  {/* 加此参数可以减小请求的图片资源大小 */}
+                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./music.png')} alt="music" />}>
+                    <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music" />
+                  </LazyLoad>
                   <div className="play_count">
                     <i className="iconfont play">&#xe885;</i>
-                    <span className="count">{getCount (item.playCount)}</span>
+                    <span className="count">{getCount(item.playCount)}</span>
                   </div>
                 </div>
                 <div className="desc">{item.name}</div>
@@ -32,6 +35,6 @@ function RecommendList (props) {
       </List>
     </ListWrapper>
   );
-  }
- 
-export default React.memo (RecommendList);
+}
+
+export default React.memo(RecommendList);
